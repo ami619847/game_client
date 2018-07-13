0f3dd3b848a4231d98ea276061e168c01b19aebd
Arjen <alphabravoab@gmail.com>
2018-07-13 15:52:51 +0200

    freakin heroku

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'postgres://qasepckmluvgrl:117f163ae915bdc6cfc71cc28e421139c7c149ea40c9c156e82cf729ed42ca87@ec2-54-217-235-16.eu-west-1.compute.amazonaws.com:5432/d5ifhp4cc9f29c'
+export const baseUrl = 'https://radiant-taiga-32148.herokuapp.com'
 
 export const localStorageJwtKey = 'currentUserJwt'
